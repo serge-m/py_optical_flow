be0f358c33cb8ecb7a53d6d56c6cbfaf0b253f29
Sergey Matyunin <matyunin.vg@gmail.com>
2014-12-29 00:18:01 +0300

    Fixed GPU usage (type of variables must be float32)

--- multiscale_of.py
+++ multiscale_of.py
@@ -7,6 +7,7 @@ import scipy.ndimage
 from train_function import TrainFunctionSimple, TrainFunctionTV
 from intermediate_saver import IntermediateSaver
 from warper import Warper
+import theano 
 
 def construct_image_pyramid(I, pyrlevels, pyrfactor):
     factor = 2. ** .5
@@ -18,10 +19,10 @@ def construct_image_pyramid(I, pyrlevels, pyrfactor):
     for m in range(pyrlevels - 1):
         shape = (numpy.array(tmp.shape) * pyrfactor).astype('int32')
         idx, idy = np.meshgrid(np.arange(shape[1]), np.arange(shape[0]))
-        idx = idx *tmp.shape[1] / shape[1].astype('float32')
-        idy = idy *tmp.shape[0] / shape[0].astype('float32')
+        idx = idx *tmp.shape[1] / shape[1].astype(theano.config.floatX)
+        idy = idy *tmp.shape[0] / shape[0].astype(theano.config.floatX)
         filt1 = scipy.ndimage.filters.gaussian_filter(tmp, smooth_sigma, order=0, output=None, mode='reflect', cval=0.0, truncate=2.0)
-        filt1r = interp2linear(filt1, idx, idy,)
+        filt1r = interp2linear(filt1, idx, idy,).astype(theano.config.floatX)
         tmp = filt1r
         pyr.append(filt1r)
     return pyr
@@ -30,8 +31,8 @@ class MultiscaleOF:
     def __init__(self, I0, I1, pyrlevels, pyrfactor, train_function_args=dict(rate=0.1, num_steps = 1000, alpha = 0.1), class_train_function = TrainFunctionTV):
         self.pyrlevels = pyrlevels
         self.pyrfactor = pyrfactor
-        self.I0pyr = construct_image_pyramid(I0, self.pyrlevels, self.pyrfactor)
-        self.I1pyr = construct_image_pyramid(I1, self.pyrlevels, self.pyrfactor)
+        self.I0pyr = construct_image_pyramid(I0.astype(theano.config.floatX), self.pyrlevels, self.pyrfactor)
+        self.I1pyr = construct_image_pyramid(I1.astype(theano.config.floatX), self.pyrlevels, self.pyrfactor)
         self.warps = 10
         self.u = None
         self.v = None
@@ -45,7 +46,7 @@ class MultiscaleOF:
                 v0 = numpy.zeros_like(self.I0pyr[level])
             else:
                 M, N = self.I0pyr[level].shape
-                rescale_v, rescale_u = numpy.array([M, N], dtype='float32') / self.I0pyr[level + 1].shape
+                rescale_v, rescale_u = numpy.array([M, N], dtype=theano.config.floatX) / self.I0pyr[level + 1].shape
                 u0 = scipy.ndimage.zoom(self.u, [rescale_v, rescale_u], order=3) * rescale_u
                 v0 = scipy.ndimage.zoom(self.v, [rescale_v, rescale_u], order=3) * rescale_v
                 if u0.shape != self.I0pyr[level].shape or v0.shape != self.I0pyr[level].shape:
